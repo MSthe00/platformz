3c4ef61d44c01c4523ea3cd37445066b263c48dc
MSthe00 <marcoschoeb@gmail.com>
2018-05-24 19:42:06 +0200

    finished timer, tweaked colors, fixed shadows, player is now bigger

--- platformz/Assets/Scripts/TimerLogic.cs
+++ platformz/Assets/Scripts/TimerLogic.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimerLogic : MonoBehaviour {
 
-    static double time; 
+    static double time;
+    public Text endscore;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,8 +15,23 @@ public class TimerLogic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        time += Time.deltaTime;
-        this.GetComponent<Text>().text = time.ToString("N2");
+        if(SceneManager.GetActiveScene().name == "Endscreen")
+        {
+            this.GetComponent<Text>().text = time.ToString("N2");
+            endscore.text = time.ToString("N2");
+          
+        }
+        else if (SceneManager.GetActiveScene().name == "Main Menu")
+        {
+            time = 0;
+        }
+        else
+        {
+
+            time += Time.deltaTime;
+            this.GetComponent<Text>().text = time.ToString("N2");
+        }
+
         
 	}
 }
